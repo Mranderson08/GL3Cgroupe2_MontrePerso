56e681fbb58d60a6ef1b70e1856973e192235556
Mranderson08 <stephanderson118@gmail.com>
2022-02-02 17:57:01 +0100

    debut des test pour deux methodes

--- src/test/java/mypackage/MontreTest.java
+++ src/test/java/mypackage/MontreTest.java
@@ -2,9 +2,15 @@ package mypackage;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class MontreTest {
+    Personne P = new Personne("Amos");
+    Montre m = new Montre(9,25);
 
     @Test
     void testAvance() {
+        P.porteMontre(m);
+        assertNotNull(P.Pmontre);
     }
 }
