71352279c0afe13aa08775e03a787e0c1ea5e018
Mranderson08 <stephanderson118@gmail.com>
2022-02-02 15:57:18 +0100

    premier commit

---
+++ src/test/java/mypackage/PersonneTest.java
@@ -0,0 +1,23 @@
+package mypackage;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class PersonneTest {
+
+    @Test
+    void testPorteMontre() {
+    }
+
+    @Test
+    void enleveMontre() {
+    }
+
+    @Test
+    void demanderHeure() {
+    }
+
+    @Test
+    void donneHeure() {
+    }
+}
